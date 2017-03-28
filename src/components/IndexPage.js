28f7b9977f49a34e1be8250fde2ff294691c984e
eariassoto <ariassotoemmanuel@gmail.com>
2017-03-27 21:19:58 -0600

    Work example index

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,11 +1,51 @@
 import React from 'react';
+
+import Box from 'grommet/components/Box';
+import Columns from 'grommet/components/Columns';
+import Section from 'grommet/components/Section';
+import Heading from 'grommet/components/Heading';
+import Paragraph from 'grommet/components/Paragraph';
 //import BookPreview from './BookPreview';
 //import books from '../data/books';
 
 export default class IndexPage extends React.Component {
   render() {
     return (
-      <h2>This is working</h2>
+      <Section>
+      <Heading strong={false} align='center'>
+        It Is Working!
+      </Heading>
+      <Columns alignContent='center'>
+  <Box align='center'
+    pad='medium'
+    margin='small'
+    colorIndex='light-2'>
+    <Paragraph align='center'>
+    Lorizzle you son of a bizzle dolor dang doggy, crazy adipiscing elizzle.
+    Get down get down shiznit velit, shizzle my nizzle crocodizzle volutpizzle,
+    suscipit check it out, gravida bow wow wow, the bizzle.
+    Pellentesque check it out ass da bomb. In hac habitasse crazy dictumst.
+    My shizz dapibizzle. Yo mamma tellus urna, shizzlin dizzle dawg, mattis
+    hizzle, eleifend vitae, nunc. Yo suscipizzle. Integizzle semper
+    crunk things brizzle.
+    </Paragraph>
+  </Box>
+  <Box align='center'
+    pad='medium'
+    margin='small'
+    colorIndex='light-2'>
+    <Paragraph align='center'>
+    Lorizzle you son of a bizzle dolor dang doggy, crazy adipiscing elizzle.
+    Get down get down shiznit velit, shizzle my nizzle crocodizzle volutpizzle,
+    suscipit check it out, gravida bow wow wow, the bizzle.
+    Pellentesque check it out ass da bomb. In hac habitasse crazy dictumst.
+    My shizz dapibizzle. Yo mamma tellus urna, shizzlin dizzle dawg, mattis
+    hizzle, eleifend vitae, nunc. Yo suscipizzle. Integizzle semper
+    crunk things brizzle.
+    </Paragraph>
+  </Box>
+</Columns>
+      </Section>
     );
   }
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 //import styles
-import 'grommet/scss/vanilla/index';
+import 'grommet/scss/grommet-core/index';
 
 import React from 'react';
 import { Link } from 'react-router';
@@ -10,14 +10,43 @@ import Box from 'grommet/components/Box';
 import Header from 'grommet/components/Header';
 import Footer from 'grommet/components/Footer';
 import Title from 'grommet/components/Title';
+import Search from 'grommet/components/Search';
+import Menu from 'grommet/components/Menu';
+import Anchor from 'grommet/components/Anchor';
+
+import Actions from 'grommet/components/icons/base/Actions';
 
 export default class Layout extends React.Component {
   render() {
     return (
       <App centered={false}>
-        <Header direction="row" justify="between" large={true}
-          pad={{horizontal: 'medium'}}>
-          <Title>My favorite books</Title>
+      <Header>
+        <Title>
+          My Favorite Books
+        </Title>
+        <Box flex={true}
+          justify='end'
+          direction='row'
+          responsive={false}>
+          <Search inline={true}
+            fill={true}
+            size='medium'
+            placeHolder='Search'
+            dropAlign={{"right": "right"}} />
+          <Menu icon={<Actions />}
+            dropAlign={{"right": "right"}}>
+            <Anchor href='#'
+              className='active'>
+              First
+            </Anchor>
+            <Anchor href='#'>
+              Second
+            </Anchor>
+            <Anchor href='#'>
+              Third
+            </Anchor>
+          </Menu>
+        </Box>
         </Header>
         <div className="app-content">{this.props.children}</div>
         <Footer primary={true} appCentered={true} direction="column"
